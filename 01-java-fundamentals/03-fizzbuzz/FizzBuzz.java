8825eb27c9e4c5d2c0c34704b3fb338bedf717f9
Kevin <kevinand1113@gmail.com>
2021-06-08 23:53:08 -0600

    FizzBuzz Upload

---
+++ 01-java-fundamentals/03-fizzbuzz/FizzBuzz.java
@@ -0,0 +1,23 @@
+public class FizzBuzz {
+    public String fizzBuzz(int number) {
+        if (number % 3 == 0 && number % 5 == 0){
+            return "FizzBuzz";
+        }
+        else if (number % 3 == 0){
+            return "Fizz";
+        }
+        else if (number % 5 == 0){
+            return "Buzz";
+        } 
+        else{
+            return Integer.toString(number);
+        }
+    }
+
+    public void counter(){
+        for(int i = 1; i <= 100; i++){
+            String result = fizzBuzz(i);
+            System.out.println("Number: " + i + " Result: " + result);
+        }
+    }
+}
---
+++ 01-java-fundamentals/03-fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,6 @@
+public class FizzBuzzTest {
+    public static void main(String[] args){
+        FizzBuzz iD = new FizzBuzz();
+        iD.counter();
+    }
+}
